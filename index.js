0058031934f5e9aa281b14566a904025f62c25c1
naman-techwink <naman.jay@techwink.net>
2025-02-02 20:46:21 +0530

    fr

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(passport.session());
 configurePassport({
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  googleCallbackURL: "http://localhost:8000/api/auth/google/callback",
+  googleCallbackURL: "https://bychatbot-server.onrender.com/api/auth/google/callback",
   passport
 })
 
